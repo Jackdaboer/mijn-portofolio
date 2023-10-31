89c7996a498666dad83f3b2c2a7654b92f633971
Jackdaboer <149495861+Jackdaboer@users.noreply.github.com>
2023-10-31 14:31:59 +0100

    normal

---
+++ normal/obstacle.js
@@ -0,0 +1,39 @@
+class Obstacle {
+
+  constructor() {
+    //this.h = 50
+    //this.w = 20
+    this.r = 200;
+    this.x = width;
+    this.y = height - this.r;
+  }
+
+  move() {
+    this.x -= 7;
+  }
+
+  show() {
+    //rect(this.x, this.y, this.w, this.h)
+    let a = random(1,4);
+    let i = round(a);
+    let img;
+
+    if (i == 1) {
+      img = bvj_img;
+      //image(bvj_img, this.x, this.y, this.r, this.r);
+    }
+    else if (i == 2) {
+      img = nova_img;
+      //image(nova_img, this.x, this.y, this.r, this.r);
+    }
+    else if (i == 3) {
+      img = paso_img;
+      //image(paso_img, this.x, this.y, this.r, this.r);
+    }
+    else if (i == 4) {
+      img = nl_img;
+      //image(nl_img, this.x, this.y, this.r, this.r);
+    }
+    image(img, this.x, this.y, this.r, this.r);
+  }
+}
